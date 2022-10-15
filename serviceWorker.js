807b24f32244836b6f772f2efc85e17371338007
Vitor <vitor_cirqueira1@hotmail.com>
2022-10-15 15:36:09 -0300

    fix sw

--- serviceWorker.js
+++ serviceWorker.js
@@ -19,3 +19,22 @@ self.addEventListener('install', installEvent => {
     })
   )
 })
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    (async function () {
+      try {
+        var res = await fetch(event.request)
+        var cache = await caches.open('cache')
+        cache.put(event.request.url, res.clone())
+        return res
+      } catch (error) {
+        return caches.match(event.request)
+      }
+    })()
+  )
+})
+
+this.addEventListener('fetch', function (event) {
+  // it can be empty if you just want to get rid of that error
+})
