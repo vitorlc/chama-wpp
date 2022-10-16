5ffe8bcdf620ffc85db35761d502ff75e34160f2
Vitor de Lima <vitor_cirqueira1@hotmail.com>
2022-10-16 19:57:35 -0300

    Release v1.1 (#1)
    * add: texto descrição
    
    * Add input option
    
    * Add install button
    
    * Fix install button
    
    * fix install button
    
    * fix install button
    
    * fix sw
    
    * fix sw
    
    * fix show/hide install button
    
    * fix display step/install
    
    * fix display step/button
    
    * fix css

---
+++ js/app.js
@@ -0,0 +1,100 @@
+// Register Service Worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker
+      .register('/serviceWorker.js')
+      .then(res => {
+        console.log('service worker registered')
+      })
+      .catch(err => console.log('service worker not registered', err))
+  })
+}
+
+// Request Notification Permission
+if (Notification.permission === 'default') {
+  Notification.requestPermission().then(permission => {
+    if (permission === 'denied') {
+      console.log('notification permission denied')
+    }
+  })
+}
+
+const openWpp = number => {
+  const DDIBrazil = '55'
+  let parsedNumber = number.replace(/\D+/g, '').replace(' ', '')
+  if (number.length == 9) parsedNumber = DDIBrazil + parsedNumber
+  window.open(`https://api.whatsapp.com/send?phone=${parsedNumber}`)
+}
+
+// On Share Open
+window.addEventListener('DOMContentLoaded', () => {
+  const parsedUrl = new URL(window.location)
+  const text = parsedUrl.searchParams.get('text')
+  console.log('Title shared: ' + parsedUrl.searchParams.get('title'))
+  console.log('Text shared: ' + text)
+  console.log('URL shared: ' + parsedUrl.searchParams.get('url'))
+  if (!text) return
+  openWpp(text)
+})
+
+// Check if webapp is instaled
+const stepElement = document.getElementById('step')
+const installButton = document.getElementById('installBtn')
+stepElement.style.display = 'none'
+installButton.style.display = 'block'
+
+const isInStandaloneMode =
+  window.matchMedia('(display-mode: standalone)').matches ||
+  window.navigator.standalone ||
+  document.referrer.includes('android-app://')
+
+if (isInStandaloneMode) {
+  stepElement.style.display = 'block'
+  installButton.style.display = 'none'
+}
+
+// Config Phone Input
+const phoneInputField = document.querySelector('#phone')
+const phoneInput = window.intlTelInput(phoneInputField, {
+  initialCountry: 'br',
+  preferredCountries: ['br', 'us'],
+  utilsScript:
+    'https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js'
+})
+
+const info = document.querySelector('.alert-info')
+const error = document.querySelector('.alert-error')
+
+function process (event) {
+  event.preventDefault()
+
+  const phoneNumber = phoneInput.getNumber()
+
+  error.style.display = 'none'
+
+  if (phoneInput.isValidNumber()) {
+    openWpp(phoneNumber)
+  } else {
+    error.style.display = ''
+    error.innerHTML = `Número de telefone inválido.`
+  }
+}
+
+const form = document.getElementById('lookup')
+form.addEventListener('submit', process)
+
+// Button install App
+let deferredPrompt
+window.addEventListener('beforeinstallprompt', e => {
+  deferredPrompt = e
+})
+
+installButton.addEventListener('click', async () => {
+  if (deferredPrompt !== null) {
+    deferredPrompt.prompt()
+    const { outcome } = await deferredPrompt.userChoice
+    if (outcome === 'accepted') {
+      deferredPrompt = null
+    }
+  }
+})
--- serviceWorker.js
+++ serviceWorker.js
@@ -3,7 +3,7 @@ const assets = [
   '/',
   '/index.html',
   '/style.css',
-  '/app.js',
+  '/js/app.js',
   'images/android-launchericon-48-48.png',
   'images/android-launchericon-72-72.png',
   'images/android-launchericon-96-96.png',
@@ -18,4 +18,23 @@ self.addEventListener('install', installEvent => {
       cache.addAll(assets)
     })
   )
+})
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    (async function () {
+      try {
+        var res = await fetch(event.request)
+        var cache = await caches.open('cache')
+        cache.put(event.request.url, res.clone())
+        return res
+      } catch (error) {
+        return caches.match(event.request)
+      }
+    })()
+  )
+})
+
+this.addEventListener('fetch', function (event) {
+  // it can be empty if you just want to get rid of that error
 })
